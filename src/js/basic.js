7666f474cffde276ec58aea1c159d1a72daf2d65
natasha80 <61574584+natasha80@users.noreply.github.com>
2021-08-05 10:52:47 +0700

    initial commit

--- src/js/basic.js
+++ src/js/basic.js
@@ -1,13 +1,13 @@
 export default function getAvailableAttackOptions(obj) {
-    const { special } = obj;
-    const result = [];
-    special.forEach((elem) => {
-      const {
-        id, name, icon, description = 'Описание недоступно',
-      } = elem;
-      result.push({
-        id, name, icon, description,
-      });
+  const { special } = obj;
+  const result = [];
+  special.forEach((elem) => {
+    const {
+      id, name, icon, description = 'Описание недоступно',
+    } = elem;
+    result.push({
+      id, name, icon, description,
     });
-    return result;
+  });
+  return result;
 }
