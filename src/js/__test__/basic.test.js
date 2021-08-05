571d43246b69c5966a3acb8224783cc089d5a09a
natasha80 <61574584+natasha80@users.noreply.github.com>
2021-08-05 08:13:50 +0700

    initial commit

---
+++ src/js/__test__/basic.test.js
@@ -0,0 +1,42 @@
+import getAvailableAttackOptions from '../basic';
+
+const character = {
+  name: 'Лучник',
+  type: 'Bowman',
+  health: 50,
+  level: 3,
+  attack: 40,
+  defence: 10,
+  special: [
+    {
+      id: 8,
+      name: 'Двойной выстрел',
+      icon: 'http://...',
+      description: 'Двойной выстрел наносит двойной урон',
+    },
+    {
+      id: 9,
+      name: 'Нокаутирующий удар',
+      icon: 'http://...',
+      // <- обратите внимание, описание "засекречено"
+    },
+  ],
+};
+
+test('The function should return an array of objects with fields (id, name, description, icon), if there is no description, the default value "Описание недоступно" is set', () => {
+  const expected = [
+    {
+      id: 8,
+      name: 'Двойной выстрел',
+      icon: 'http://...',
+      description: 'Двойной выстрел наносит двойной урон',
+    },
+    {
+      id: 9,
+      name: 'Нокаутирующий удар',
+      icon: 'http://...',
+      description: 'Описание недоступно',
+    },
+  ];
+  expect(getAvailableAttackOptions(character)).toEqual(expected);
+});
